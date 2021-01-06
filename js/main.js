533d7b297287552eb1762e5e731d7b842b3a7b5f
pavedom13 <pavedom13@gmail.com>
2021-01-06 17:24:56 +0300

    изменения в js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	$('.nav__burger').click(function(event) {
         $('.nav__burger,.nav__menu').toggleClass('nav__menu-active');
-        //запрет скрола контента при открытом меню
-		$('body').toggleClass('lock');
 	});
 });
